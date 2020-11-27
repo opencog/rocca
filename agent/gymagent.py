8783e6d7603e14bbb962d0188e7b84b42809a74d
Nil Geisweiller <ngeiswei@gmail.com>
2020-11-27 09:15:11 +0200

    Complete Bayesian Model Averaging (weights in the mixture model)

--- agent/gymagent.py
+++ agent/gymagent.py
@@ -7,7 +7,9 @@
 # Python
 import os
 import time
+import math
 from orderedmultidict import omdict
+import scipy.special as sp
 
 # OpenCog
 from opencog.atomspace import AtomSpace, TruthValue
@@ -25,8 +27,6 @@ from .utils import *
 # Constants #
 #############
 
-TRUE_TV = TruthValue(1, 1)
-DEFAULT_TV = TruthValue(1, 0)
 X_ENABLED = 'DISPLAY' in os.environ
 
 #########
@@ -46,6 +46,25 @@ class GymAgent:
         self.step_count = 0
         self.accumulated_reward = 0
         self.percepta_record = ConceptNode("Percepta Record")
+
+        # Parameters controlling learning and decision
+
+        # Prior alpha and beta of beta-distributions corresponding to
+        # truth values
+        self.prior_a = 1.0
+        self.prior_b = 1.0
+
+        # Parameter to control the complexity penalty over the
+        # cognitive schematics. Ranges from 0, no penalty to +inf,
+        # infinit penalty. Affect the calculation of the cognitive
+        # schematic prior.
+        self.cpx_penalty = 1.0
+
+        # Parameter to estimate the length of a whole model given a
+        # partial model + unexplained data. Ranges from 0 to 1, 0
+        # being no compressiveness at all of the unexplained data, 1
+        # being full compressiveness.
+        self.compressiveness = 0.9
 
     def __del__(self):
         self.env.close()
@@ -198,6 +217,151 @@ class GymAgent:
         # TODO
         return []
 
+    # TODO: move to its own class (MixtureModel or something)
+    def get_all_uniq_atoms(self, atom):
+        """Return the set of all unique atoms in atom.
+
+        """
+
+        # Base cases
+        if atom.is_node():
+            return {atom}
+
+        # Recursive cases
+        if atom.is_link():
+            results = {atom}
+            for o in atom.out:
+                results.union(self.get_all_uniq_atoms(o))
+            return results
+
+    # TODO: move to its own class (MixtureModel or something)
+    def complexity(self, atom):
+        """Return the count of all unique atoms in atom.
+
+        """
+
+        return len(self.get_all_uniq_atoms(atom))
+
+    # TODO: move to its own class (MixtureModel or something)
+    def prior(self, length):
+        """Given the length of a model, calculate its prior.
+
+        Specifically
+
+        exp(-cpx_penalty*length)
+
+        where cpx_penalty is a complexity penalty parameter (0 for no
+        penalty, +inf for infinit penalty), and length is the size of
+        the model, the total number of atoms involved in its
+        definition.
+
+        The prior doesn't have to sum up to 1 because the probability
+        estimates are normalized.
+
+        """
+
+        return math.exp(-self.cpx_penalty * length)
+
+    # TODO: move to its own class (MixtureModel or something)
+    def kolmogorov_estimate(self, remain_count):
+        """Given the size of the data set that isn't explained by a model,
+        estimate the complexity of a model that would explain them
+        perfectly. The heuristic used here is
+
+        remain_data_size^(1 - compressiveness)
+
+        If compressiveness is null, then no compression occurs, the
+        model is the data set itself, if compressiveness equals 1,
+        then it return 1, which is the maximum compression, all data
+        can be explained with just one bit.
+
+        """
+
+        return pow(remain_count, 1.0 - self.compressiveness)
+
+    # TODO: move to its own class (MixtureModel or something)
+    def prior_estimate(self, cogscm):
+        """Calculate the prior probability of cogscm.
+
+        """
+
+        partial_complexity = self.complexity(cogscm)
+        remain_data_size = self.data_set_size - cogscm.tv.count
+        kestimate = self.kolmogorov_estimate(remain_data_size);
+        return self.prior(partial_complexity + kestimate);
+
+    # TODO: move to its own class (MixtureModel or something)
+    def beta_factor(self, cogscm):
+        """Return the beta factor as described in Eq.26 of
+
+        https://github.com/ngeiswei/papers/blob/master/PartialBetaOperatorInduction/PartialBetaOperatorInduction.pdf
+
+        Note that we do account for the normalizing factor (numerator
+        of the fraction of Eq.22) as its not clear we want to
+        normalize the BMA (we might want to replace rest by an unknown
+        distribution, i.e. a prior beta-distribution).
+
+        """
+
+        a = tv_to_alpha_param(cogscm.tv, self.prior_a, self.prior_b)
+        b = tv_to_beta_param(cogscm.tv, self.prior_a, self.prior_b)
+        return sp.beta(a, b) / sp.beta(self.prior_a, self.prior_b)
+
+    # TODO: move to its own class (MixtureModel or something)
+    def weight(self, cogscm):
+        """Calculate the weight of a cogscm for Model Bayesian Averaging.
+
+        The calculation is based on
+
+        https://github.com/ngeiswei/papers/blob/master/PartialBetaOperatorInduction/PartialBetaOperatorInduction.pdf
+
+        It assumes self.data_set_size has been properly set.
+
+        """
+
+        return self.prior_estimate(cogscm) * self.beta_factor(cogscm)
+
+    # TODO: move to its own class (MixtureModel or something)
+    def infer_data_set_size(self, cogscms):
+        """Infer the data set size by taking the max count of all models
+
+        (it works assuming that one of them is complete).
+
+        """
+
+        if 0 < len(cogscms):
+            return max(cogscms, key=lambda x : x.tv.count).tv.count
+        else:
+            return 0
+
+    def w8d_cogscm_to_str(self, w8d_cogscm, indent=""):
+        """Pretty print the given list of weighted cogscm"""
+
+        weight = w8d_cogscm[0]
+        cogscm = w8d_cogscm[1]
+        tv = get_cogscm_tv(cogscm)
+        idstr = cogscm.id_string() if cogscm else "None"
+        s = "(weight={}, tv={}, id={})".format(weight, tv, idstr)
+        return s
+
+    def w8d_cogscms_to_str(self, w8d_cogscms, indent=""):
+        """Pretty print the given list of weighted cogscms"""
+
+        w8d_cogscms_sorted = sorted(w8d_cogscms, key=lambda x : x[0], reverse=True)
+
+        s = ""
+        for w8d_cogscm in w8d_cogscms_sorted:
+            s += indent + self.w8d_cogscm_to_str(w8d_cogscm, indent + "  ") + "\n"
+        return s
+
+    def mxmdl_to_str(self, mxmdl, indent=""):
+        """Pretty print the given mixture model of cogscms"""
+
+        s = ""
+        for act_w8d_cogscms in mxmdl.listitems():
+            s += "\n" + indent + str(act_w8d_cogscms[0]) + "\n"
+            s += self.w8d_cogscms_to_str(act_w8d_cogscms[1], indent + "  ")
+        return s
 
     def deduce(self, cogscms):
         """Return an action distribution given a list cognitive schematics.
@@ -276,25 +440,32 @@ class GymAgent:
         valid_cogscms = [cogscm for cogscm in cogscms if 0.9 < ctx_tv(cogscm).mean]
         log.debug("valid_cogscms = {}".format(valid_cogscms))
 
-        # For now we have a uniform weighting across valid cognitive
+        # Size of the complete data set, including all observations
+	# used to build the models. For simplicity we're gonna assume
+	# that it is the max of all counts over the models. Meaning
+	# that to do well, at least one model has to be complete,
+	# however bad this model might be.
+        #
+        # Needs to be set before calling self.weight
+        self.data_set_size = self.infer_data_set_size(valid_cogscms)
+
+        # For each action, map a list of weighted valid cognitive
         # schematics.
-        actdist = omdict([(get_action(cogscm), (1.0, cogscm.tv))
-                          for cogscm in valid_cogscms])
-        log.debug("actdist-1 = {}".format(actdist))
+        mxmdl = omdict([(get_action(cogscm), (self.weight(cogscm), cogscm))
+                     for cogscm in valid_cogscms])
 
         # Add an unknown component for each action. For now its weight
         # is constant, delta, but ultimately is should be calculated
         # as a rest in the Solomonoff mixture.
-        delta = 0.5
+        delta = 1.0e-3
         for action in self.atomese_action_space():
-            actdist.add(action, (delta, DEFAULT_TV))
-        log.debug("actdist-2 = {}".format(actdist))
+            mxmdl.add(action, (delta, None))
 
-        return actdist
+        return mxmdl
 
 
-    def decide(self, actdist):
-        """Select the next action to enact from an action distribution.
+    def decide(self, mxmdl):
+        """Select the next action to enact from a mixture model of cogscms.
 
         The action is selected from the action distribution, a list of
         pairs (action, tv), obtained from deduce.  The selection uses
@@ -307,7 +478,7 @@ class GymAgent:
 
         # Select the pair of action and its first order probability of
         # success according to Thompson sampling
-        (action, pblty) = thompson_sample(actdist)
+        (action, pblty) = thompson_sample(mxmdl, self.prior_a, self.prior_b)
 
         # Return the action (we don't need the probability for now)
         return (action, pblty)
@@ -339,11 +510,11 @@ class GymAgent:
         log.debug("css = {}".format(css))
 
         # Deduce the action distribution
-        actdist = self.deduce(css)
-        log.debug("actdist = {}".format(actdist))
+        mxmdl = self.deduce(css)
+        log.debug("mxmdl = {}".format(self.mxmdl_to_str(mxmdl)))
 
         # Select the next action
-        action, pblty = self.decide(actdist)
+        action, pblty = self.decide(mxmdl)
         log.debug("(action={}, pblty={})".format(action, pblty))
 
         # Timestamp the action that is about to be executed
