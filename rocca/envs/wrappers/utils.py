f44c50a6a2d395b6bb1e80263464ddd7a4988092
Jade O'Neill <jadeoneillpk@gmail.com>
2021-07-13 00:32:14 -0400

    Firstly, add a feature detector to make the reward function understandable by the pattern miner. Secondly, add a simple and efficient computer vision function.

--- rocca/envs/wrappers/utils.py
+++ rocca/envs/wrappers/utils.py
@@ -6,6 +6,7 @@ from fastcore.basics import listify
 from opencog.atomspace import Atom, is_a
 from opencog.atomspace import types as AT
 from opencog.type_constructors import *
+from opencog.spacetime import *
 
 
 def mk_action(name, value):
@@ -23,6 +24,50 @@ def mk_node(name):
         return ConceptNode(name)
     raise RuntimeError("Error: Unknown type.")
 
+def convert_percept(predicate, *args):
+    """Firstly, the MineRL environment gives us a different floating point
+    reward number every step. This function converts it into +1 or -1 so that
+    the pattern miner can find frequent patterns involving reward.
+    
+    Secondly, MineRL gives us a 2D image of the agent's view within Minecraft,
+    but this function gives the agent the average location of each approximate
+    color on screen. The colors are put in bins of 20 pixel brightness values,
+    and then we record the average location of the color bin on screen. Note
+    that this function doesn't divide the screen into blobs of one color; it
+    may find the average of multiple blobs of one color. Note that the pattern
+    miner will still have difficulty with this feature so it's a work in
+    progress."""
+    if predicate == "pov":
+        #print (args, type(args))
+        #args = ["some image"]
+        from collections import defaultdict
+        colors = defaultdict(list)
+        
+        for y in range(0, 64):
+            for x in range(0, 64):
+                color = args[y][x]
+                rounded_color = tuple([subpixel // 25 * 25 for subpixel in color])
+                colors[rounded_color].append((x, y))
+        
+        #print(f"{len(colors.keys())} colors in this frame")
+        #args = ["some image"]
+        links = []
+        for (color, locations) in colors.items():
+            total_x = total_y = 0
+            for (x, y) in locations:
+                total_x += x
+                total_y += y
+            links.append(AtLocationLink(mk_node("color:"+str(color)), mk_node("viewLocation:"+str((total_x//len(locations), total_y//len(locations))))))
+        #print(links)
+        return links
+    
+    elif predicate == "Reward":
+        if float(args[0]) > 0:
+            args = [1]
+        elif float(args[0]) < 0:
+            args = [-1]
+
+    return [mk_evaluation(predicate, *args)]
 
 def mk_evaluation(predicate, *args):
     pred = PredicateNode(predicate)
