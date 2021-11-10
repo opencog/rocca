7ec3b77f5f67c1a37ed14ba5b7235a860c252f14
Hedra <tanksh24@gmail.com>
2021-11-10 17:49:11 -0500

    Fixes to chase_malmo example

--- rocca/envs/wrappers/__init__.py
+++ rocca/envs/wrappers/__init__.py
@@ -3,4 +3,4 @@ from .cartpole_wrapper import CartPoleWrapper
 from .gym_wrapper import GymWrapper
 from .minerl_wrapper import MineRLWrapper
 
-# from .malmo_wrapper import MalmoWrapper
+from .malmo_wrapper import MalmoWrapper
