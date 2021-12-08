57b4106bd0a4d3eb409d50364dbcb8100eb3d536
Nil Geisweiller <ngeiswei@gmail.com>
2021-12-08 11:42:24 +0200

    Add move smoothly and jump

--- experiments/minetest-xp.py
+++ experiments/minetest-xp.py
@@ -19,18 +19,28 @@
 # studying https://github.com/MaikePaetzel/minetest-agent
 
 import copy
+import time
 import miney
 
+# Delay between each test (in second)
+test_delay = 1.0
+def log_and_wait(msg : str):
+    print("============ {} ============".format(msg))
+    time.sleep(test_delay)
+
 # Connect Minetest
+log_and_wait("Connect to minetest")
 mt = miney.Minetest()
 print("mt = {}".format(mt))
 
-# We set the time to midnight
+# We set the time to mid-day
+log_and_wait("Set time to mid-day")
 print("mt.time_of_day = {}".format(mt.time_of_day))
 mt.time_of_day = 0.5
 print("mt.time_of_day = {}".format(mt.time_of_day))
 
 # Set Lua interpreter
+log_and_wait("Get lua interpreter")
 lua = miney.Lua(mt)
 print("lua = {}".format(lua))
 
@@ -42,11 +52,14 @@ print("lua = {}".format(lua))
 # List of unsupported Python constructs (so far):
 # - set
 # - tuple
+log_and_wait("Convert python data to lua")
 data = ["123", "abc"]
 data_dumps = lua.dumps(data)
-print("data_dumps = {}".format(data_dumps))
+print("python data = {}".format(data))
+print("lua data_dumps = {}".format(data_dumps))
 
 # Run simple lua code
+log_and_wait("Invoke minetest lua interpreter to calculate 1+2")
 simple_lua_code = "return 1 + 2"
 simple_lua_result = lua.run(simple_lua_code)
 print("simple_lua_result = {}".format(simple_lua_result))
@@ -95,6 +108,7 @@ print("simple_lua_result = {}".format(simple_lua_result))
 # - int LuaLocalPlayer::l_get_control(lua_State *L) (would be nice if we had set_control)
 
 # Chat
+log_and_wait("Send chat to minetest")
 chat = """
 return minetest.chat_send_all(\"Hello Minetest\")
 """
@@ -105,6 +119,7 @@ print("chat_result = {}".format(chat_result))
 player_name = "singleplayer"
 
 # Check that the player exists
+log_and_wait("Check that player exists")
 player_exists = """
 return minetest.player_exists(\"{}\")
 """.format(player_name)
@@ -112,37 +127,46 @@ player_exists_result = lua.run(player_exists)
 print("player_exists = {}".format(player_exists_result))
 
 # Retrieve the name of the player (given its name, hehe)
+log_and_wait("Retrieve player name")
 player_retrieve_name = """
 local player = minetest.get_player_by_name(\"{}\")
-local player_name = player:get_player_name()
-return player_name
+return player:get_player_name()
 """.format(player_name)
 player_retrieve_name_result = lua.run(player_retrieve_name)
 print("player_retrieve_name_result = {}".format(player_retrieve_name_result))
 
+# Create lua runner helper using signleplayer
+def player_lua_run(code : str, player : str = "singleplayer"):
+    get_player_code = "local player = minetest.get_player_by_name(\"{}\")".format(player)
+    full_code = get_player_code + "\n" + code
+    print("Run lua code:\n\"\"\"\n{}\n\"\"\"".format(full_code))
+    return lua.run(full_code)
+
 # Get player position
-get_player_pos = """
-local player = minetest.get_player_by_name(\"{}\")
-return player:get_pos()
-""".format(player_name)
-get_player_pos_result = lua.run(get_player_pos)
+log_and_wait("Retrieve player position")
+get_player_pos_result = player_lua_run("return player:get_pos()")
 print("get_player_pos_result = {}".format(get_player_pos_result))
 
 # Move player to a shifted position.  Setting the continuous argument
 # of move_to to true does not work for players (as explained in
 # https://github.com/minetest/minetest/blob/master/doc/lua_api.txt).
+log_and_wait("Move abruptly player to new position")
 player_shifted_pos = copy.copy(get_player_pos_result)
-player_shifted_pos['x'] += 10
+player_shifted_pos['x'] += 1
 print("player_shifted_pos = {}".format(player_shifted_pos))
-move_player_to = """
-local player = minetest.get_player_by_name(\"{}\")
-return player:move_to({})
-""".format(player_name, lua.dumps(player_shifted_pos))
-print("move_player_to = {}".format(move_player_to))
-move_player_to_result = lua.run(move_player_to)
-print("get_player_to_result = {}".format(move_player_to_result))
+move_player_to = "return player:move_to({})".format(lua.dumps(player_shifted_pos))
+move_player_to_result = player_lua_run(move_player_to)
+print("move_player_to_result = {}".format(move_player_to_result))
 
-# NEXT: smooth move with add_velocity
+# Come back smoothly to its original position
+log_and_wait("Move smoothly player to back to previous position")
+player_move_back_result = player_lua_run("return player:add_velocity({x=-6.5, y=0, z=0})")
+print("player_move_back_result = {}".format(player_move_back_result))
+
+# Jump
+log_and_wait("Jump")
+player_jump_result = player_lua_run("return player:add_velocity({x=0, y=6.5, z=0})")
+print("player_jump_result = {}".format(player_jump_result))
 
 # # Move player to the coordonates of a tree
 # tree_pos = {'y': 14.5, 'x': -302.1969909668, 'z': -200.82400512695}
